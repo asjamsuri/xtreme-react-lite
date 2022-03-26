907a4f5c20c3e123791133f347b2bec2c307df4b
asjamsuri <adythia.sjamsuri@gmail.com>
2022-03-26 10:48:46 +0700

    Add tslint command

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useRoutes } from "react-router-dom";
 import Themeroutes from "./routes/Router";
 
 const App = () => {
+
   const routing = useRoutes(Themeroutes);
 
   return <div className="dark">{routing}</div>;
